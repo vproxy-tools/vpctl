830775eaaf4dce492273c2443b418b6b31383e4d
K.G. Wang <wkgcass@hotmail.com>
2020-01-22 17:32:57 +0800

    generated code: socks5update AllowNonBackend should be bool pointer

--- gen.sh
+++ gen.sh
@@ -1,1 +1,5 @@
 GOPATH=`pwd` go-swagger generate client -f ../vproxy/doc/api.yaml --api-package=vproxy.client.api --model-package=vproxy.client.model --client-package=vproxy.client --target=./src
+
+# need to modify some generated code
+sed -i -e 's/AllowNonBackend bool `json:"allowNonBackend,omitempty"`/AllowNonBackend *bool `json:"allowNonBackend,omitempty"`/g' "src/vproxy_client_model/socks5_server_update.go"
+rm src/vproxy_client_model/socks5_server_update.go-e
