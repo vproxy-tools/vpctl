173db5189cc1f0b555cef4a33e4df25c1798b09e
K.G. Wang <wkgcass@hotmail.com>
2020-04-14 21:45:52 +0800

    upgrade k8sOp.sh

--- k8sOp.sh
+++ k8sOp.sh
@@ -4,26 +4,39 @@ op="$1"
 
 if [ -z "$op" ]
 then
-	echo "First argument should be the operation you want to perform: get|apply|stop|log-ctl|log-vp|exec-ctl|exec-vp"
+	echo "First argument should be the operation you want to perform: get|apply|stop|log|exec"
 	exit 1
 fi
 
-if [ ! -z "$2" ]
+target="$2"
+
+if [ "$op" == "log" ] || [ "$op" == "exec" ]
 then
-	echo "Too many arguments"
-	exit 1
+	if [ -z "$target" ]
+	then
+		echo "Second argument should be the operating target: ctl|vp"
+		exit 1
+	fi
+	if [ "$target" != "ctl" ] && [ "$target" != "vp" ]
+	then
+		echo "No such operating target"
+		exit 1
+	fi
+	shift 2
+else
+	shift 1
 fi
 
 if [ "$op" == "get" ]
 then
-	kubectl -n vproxy-system get pod -o wide | grep vproxy-gateway
+	kubectl -n vproxy-system get pod "$@" -o wide | grep vproxy-gateway
 elif [ "$op" == "apply" ]
 then
 	kubectl apply -f misc/k8s-vproxy.yaml
 elif [ "$op" == "stop" ]
 then
 	kubectl -n vproxy-system delete deployment vproxy-gateway
-elif [ "$op" == "log-ctl" ] || [ "$op" == "log-vp" ] || [ "$op" == "exec-ctl" ] || [ "$op" == "exec-vp" ]
+elif [ "$op" == "log" ] || [ "$op" == "exec" ]
 then
 	pod=`kubectl -n vproxy-system get pod | grep vproxy-gateway | grep Running | awk '{print $1}'`
 	c=`echo "$pod" | wc -l`
@@ -36,20 +49,45 @@ then
 		echo "More than one running vproxy-gateway"
 		exit 1
 	fi
-	if [ "$op" == "log-ctl" ]
+
+	if [ "$target" == "ctl" ]
 	then
-		kubectl -n vproxy-system logs --tail=50 -f "$pod" controller
-	elif [ "$op" == "log-vp" ]
+		container="controller"
+	elif [ "$target" == "vp" ]
 	then
-		kubectl -n vproxy-system logs --tail=50 -f "$pod" vproxy
-	elif [ "$op" == "exec-ctl" ]
-	then
-		kubectl -n vproxy-system exec -it "$pod" --container=controller /bin/bash
-	elif [ "$op" == "exec-vp" ]
-	then
-		kubectl -n vproxy-system exec -it "$pod" --container=vproxy sh
+		container="vproxy"
 	else
-		echo "BUG"
+		echo "BUG 2"
+		exit 1
+	fi
+
+	if [ "$op" == "log" ]
+	then
+		extra_log_cmd="--tail=50 -f"
+		if [ ! -z "$1" ]
+		then
+			extra_log_cmd="$@"
+		fi
+		kubectl -n vproxy-system logs $extra_log_cmd "$pod" "$container"
+	elif [ "$op" == "exec" ]
+	then
+		if [ "$target" == "ctl" ]
+		then
+			exec_cmd="/bin/bash"
+		elif [ "$target" == "vp" ]
+		then
+			exec_cmd="sh"
+		else
+			echo "BUG 3"
+			exit 1
+		fi
+		if [ ! -z "$1" ]
+		then
+			exec_cmd="$@"
+		fi
+		kubectl -n vproxy-system exec -it "$pod" --container="$container" $exec_cmd
+	else
+		echo "BUG 1"
 		exit 1
 	fi
 else
