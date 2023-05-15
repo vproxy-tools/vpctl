d541ee0b7bc0b70e3afc888368381f02ebaf8254
wkgcass <wkgcass@hotmail.com>
2023-05-15 11:30:16 +0800

    add Socks5Server

--- main.go
+++ main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "TcpLb")
 		os.Exit(1)
 	}
+	if err = (&controllers.Socks5ServerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Socks5Server")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
