706f35292aa7e4d8ed7d2635deee67bb061da424
wkgcass <wkgcass@hotmail.com>
2023-05-15 11:31:00 +0800

    add Upstream

--- main.go
+++ main.go
@@ -110,6 +110,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DnsServer")
 		os.Exit(1)
 	}
+	if err = (&controllers.UpstreamReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Upstream")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
